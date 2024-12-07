16e59fd040db39b9ea512384b11a3f909b34d38f
sam2829 <samscarisbrick@outlook.com>
2024-12-07 11:37:57 +0000

    Created and set up profiles app and model

--- control_systems_jobs/settings.py
+++ control_systems_jobs/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+if os.path.exists('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'profiles',
 ]
 
 MIDDLEWARE = [
---
+++ profiles/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    """
+    Profile class which is created automatically when user created
+    """
+
+    # List of location options
+    WORKSHOP = 'Workshop'
+    SYSPAL = 'Syspal'
+    
+    LOCATION_CHOICES = [
+        (WORKSHOP, 'Workshop'),
+        (SYSPAL, 'Syspal'),
+    ]
+
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=50, blank=True)
+    work_location = models.CharField(
+        max_length=15, choices=LOCATION_CHOICES, default='WORKSHOP'
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
+    
+# Code to create new profile everytime new user is created
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+post_save.connect(create_profile, sender=User)

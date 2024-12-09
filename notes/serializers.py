61551d50c45ad324a0d4180a8cce57e90d600978
sam2829 <samscarisbrick@outlook.com>
2024-12-09 16:16:42 +0000

    Add functionality for NoteDetail views, serializers and urls

--- notes/serializers.py
+++ notes/serializers.py
@@ -28,3 +28,11 @@ class NoteSerializer(serializers.ModelSerializer):
             'id', 'owner', 'job', 'created_at', 'updated_at', 'content',
             'is_owner'
         ]
+
+
+class NoteDetailSerializer(NoteSerializer):
+    """
+    This class is to serialize note detail data. Job is a
+    read only field so we don't have to set it on each update
+    """
+    job = serializers.ReadOnlyField(source='job.id')
--- notes/views.py
+++ notes/views.py
@@ -2,7 +2,7 @@ from rest_framework import generics
 from .models import Note
 from rest_framework.permissions import IsAuthenticated
 from control_systems_jobs.permissions import IsOwnerOrReadOnly
-from .serializers import NoteSerializer
+from .serializers import NoteSerializer, NoteDetailSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 
 
@@ -23,3 +23,14 @@ class NoteList(generics.ListCreateAPIView):
     #create owner field when note created
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+
+class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    This class is to render a single note, and be able to
+    update and delete if owner or superuser
+    """
+    
+    serializer_class = NoteDetailSerializer
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+    queryset = Note.objects.all()

b5f3e8526c38035796703c6564e5bc6bd97882d6
sam2829 <samscarisbrick@outlook.com>
2024-12-09 08:25:30 +0000

    Add functionality so only a superuser can change the work location on users profiles

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -12,6 +12,21 @@ class ProfileSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+    
+    # function for only letting superuser edit work location
+    def update(self, instance, validated_data):
+        request = self.context['request']
+
+        # check if work location is being changed
+        if 'work_location' in validated_data:
+            if validated_data['work_location'] != instance.work_location:
+                # check if superuser
+                if not request.user.is_superuser:
+                    raise serializers.ValidationError(
+                        {"work_location": "Only superusers can update work location."}
+                    )
+                
+        return super().update(instance, validated_data)
 
     class Meta:
         model = Profile

285f408f6def6b419528d700474e1b687ca3d08f
sam2829 <samscarisbrick@outlook.com>
2024-12-11 15:25:52 +0000

    Update allowed hosts to correct url

--- control_systems_jobs/settings.py
+++ control_systems_jobs/settings.py
@@ -57,7 +57,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost', 'https://control-systems-jobs-new-a2b084cfda13.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost', 'control-systems-jobs-new-a2b084cfda13.herokuapp.com']
 
 
 # Application definition

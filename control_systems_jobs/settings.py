85e28af57dfd64a2d1ad3ea49078e24d5820fda3
sam2829 <samscarisbrick@outlook.com>
2024-12-11 14:04:43 +0000

    change allowed hosts

--- control_systems_jobs/settings.py
+++ control_systems_jobs/settings.py
@@ -57,7 +57,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = ['localhost','https://control-systems-jobs-8e7c07b4a83a.herokuapp.com/']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition

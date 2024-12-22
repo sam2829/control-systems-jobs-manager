bea024c42ceefb02f5a80f4df4c2e4bf5e554abc
sam2829 <samscarisbrick@outlook.com>
2024-12-22 10:51:33 +0000

    Add automated testing for profiles app

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,93 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class ProfileListViewTests(APITestCase):
+    """
+    this class is for testing the ProfileList views
+    """
+    # setup logged in user
+    def setUp(self):
+        sam = User.objects.create_user(username='sam', password='pass')
+
+    # test list of profiles are displayed
+    def test_can_list_profiles(self):
+        response = self.client.get('/api/profiles/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Ensure there's exactly one user
+        self.assertEqual(User.objects.count(), 1)
+         # Ensure there's exactly one profile
+        self.assertEqual(Profile.objects.count(), 1)
+        
+
+
+class ProfileDetailViewTests(APITestCase):
+    """
+    this class is for testing the Profile Detail views
+    """
+    # setup logged in users
+    def setUp(self):
+        self.sam = User.objects.create_user(username='sam', password='pass123')
+        self.emma = User.objects.create_user(username='emma', password='pass456')
+
+    # test that user can retrieve single profile by id
+    def test_can_retrieve_profile_detail(self):
+        response = self.client.get(f'/api/profiles/1')
+        self.assertEqual(response.data['owner'], 'sam')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # test user cannot retrieve a profile with invalid id
+    def test_cannot_retrieve_profile_detail_using_invalid_id(self):
+        response = self.client.get(f'/api/profiles/23')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    # test that user can update a profile
+    def test_logged_in_user_can_update_profile(self):
+        self.client.login(username='sam', password='pass123')
+        response = self.client.put(
+            '/api/profiles/1',
+            {
+                'name': 'sam28',
+            }
+        )
+        profile = Profile.objects.filter(pk=1).first()
+        self.assertEqual(profile.name, 'sam28')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # test that user cannot updated someone elses profile
+    def test_logged_in_user_cannot_update_anothers_profile(self):
+        self.client.login(username='sam', password='pass123')
+        response = self.client.put(
+            '/api/profiles/2',
+            {
+                'name': 'sam28',
+            }
+        )
+        profile = Profile.objects.filter(pk=2).first()
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    # test that user cannot update profile logged out
+    def test_logged_out_user_cannot_update_profile(self):
+        response = self.client.put(
+            f'/api/profiles/1',
+            {
+                'name': 'sam28',
+            }
+        )
+        profile = Profile.objects.filter(pk=1).first()
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    # test user cannot update work location
+    def test_user_cannot_update_work_location(self):
+        self.client.login(username='sam', password='sam123')
+        response = self.client.put(
+            '/api/profiles/1',
+            {
+                'name': 'sam',
+                'work_location': 'SYSPAL',
+            }
+        )
+        profile = Profile.objects.filter(pk=1).first()
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)

97e304c99a261d581eac7f7f91241262b11729d9
sam2829 <samscarisbrick@outlook.com>
2025-02-12 17:17:59 +0000

    Add styling for job detail page

--- frontend/src/pages/jobDetailPage/JobDetailPageJob.js
+++ frontend/src/pages/jobDetailPage/JobDetailPageJob.js
@@ -1,4 +1,8 @@
 import React from "react";
+import styles from "../../styles/JobDetailPageJob.module.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import CustomButton from "../../components/CustomButton";
 
 const JobDetailPageJob = ({
   id,
@@ -8,7 +12,6 @@ const JobDetailPageJob = ({
   quantity,
   description,
   quote,
-  date_created,
   workshop_status,
   syspal_status,
   delivered,
@@ -17,17 +20,71 @@ const JobDetailPageJob = ({
 }) => {
   return (
     <>
-      <p>{csa_number}</p>
-      <p>{syspal_number}</p>
-      <p>{order_number}</p>
-      <p>{quantity}</p>
-      <p>{description}</p>
-      <p>{quote}</p>
-      <p>{date_created}</p>
-      <p>{workshop_status}</p>
-      <p>{syspal_status}</p>
-      <p>{delivered ? "yes" : "No"}</p>
-      <p>{delivered_date ? delivered_date : "Not delivered"}</p>
+      <section className={`${styles.DataContainer} my-5 py-4`}>
+        <Row>
+          <Col xs={6} lg={3} className="justify-content-center">
+            <p className={styles.Text}>CSA Number:</p>
+            <p className={styles.Data}>{csa_number}</p>
+          </Col>
+          <Col xs={6} lg={3}>
+            <p className={styles.Text}>Syspal Number:</p>
+            <p className={styles.Data}>{syspal_number}</p>
+          </Col>
+          <Col xs={6} lg={3}>
+            <p className={styles.Text}>Order Number:</p>
+            <p className={styles.Data}>{order_number}</p>
+          </Col>
+          <Col xs={6} lg={3}>
+            <p className={styles.Text}>Quantity:</p>
+            <p className={styles.Data}>{quantity}</p>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={6} lg={3}>
+            <p className={styles.Text}>Workshop Status:</p>
+            <p className={styles.Data}>{workshop_status}</p>
+          </Col>
+          <Col xs={6} lg={3}>
+            <p className={styles.Text}>Syspal Status:</p>
+            <p className={styles.Data}>{syspal_status}</p>
+          </Col>
+          <Col xs={6} lg={3}>
+            <p className={styles.Text}>Date Delivered:</p>
+            <p className={styles.Data}>{delivered ? "yes" : "No"}</p>
+          </Col>
+          <Col xs={6} lg={3}>
+            <p className={styles.Text}>Date Delivered:</p>
+            <p className={styles.Data}>
+              {delivered_date ? delivered_date : "Not delivered"}
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} lg={6}>
+            <p className={styles.Text}>Description:</p>
+            <p className={styles.Data}>{description}</p>
+          </Col>
+          {/* import of custom buttons */}
+          <Col xs={12} sm={6} lg={3}>
+            <div className={`${styles.Button} py-1`}>
+              <CustomButton text="Save Changes" />
+            </div>
+          </Col>
+          <Col xs={12} sm={6} lg={3}>
+            <div className={`${styles.Button} py-1`}>
+              <CustomButton text="Cancel" />
+            </div>
+          </Col>
+        </Row>
+      </section>
+      {/* quote section */}
+      <section className="px-0">
+        <h2 className={styles.SubHeading}>Quote</h2>
+        <p className={`${styles.Data} ${styles.QuoteContainer} my-4`}>
+          {quote}
+        </p>
+      </section>
+
       <p>{notes_count}</p>
     </>
   );
